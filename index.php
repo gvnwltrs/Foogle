99a1fe4b9a0230821b326745039cb42f8db7c241
Gavin Walters <gwalters@pop-os.localdomain>
2022-08-15 22:45:04 -0400

    initial commit with index page and a foundation to work from

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>Welcome to Foogle</title>
+</head>
+<body>
+
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="assets/images/foogle-logo.png">
+        </div>
+
+        <div class="searchContainer">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+        </div>
+    </div>
+
+</body>
+</html>
