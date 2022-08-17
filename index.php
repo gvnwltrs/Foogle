30e096f15303772d254df336ef665dd68b4d09d4
Gavin Walters <gwalters@pop-os.localdomain>
2022-08-16 21:25:13 -0400

    added logo and styled page buttons and search bar

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <head>
     <title>Welcome to Foogle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
 
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="assets/images/foogle-logo.png">
+    <div class="wrapper indexPage">
+
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="assets/images/foogle-logo1.png">
+            </div>
+
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
+            </div>
         </div>
 
-        <div class="searchContainer">
-            <input class="searchBox" type="text" name="term">
-            <input class="searchButton" type="submit" value="Search">
-        </div>
     </div>
 
 </body>
