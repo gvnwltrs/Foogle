fc96e25f0d3483da859d0e863eea0036dc298d8f
Gavin Walters <gwalters@pop-os.localdomain>
2022-08-20 17:04:40 -0400

    improved web crawler, grabbing meta tags

---
+++ crawl.php
@@ -0,0 +1,122 @@
+<?php 
+require_once("classes/DomDocumentParser.php"); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$alreadyCrawled = array(); 
+$crawling = array();
+
+function createLink($src, $url) {
+    $scheme = parse_url($url)["scheme"]; // http
+    $host = parse_url($url)["host"]; // www.gavinwalters.com 
+    // src example: /about.php 
+
+    if(substr($src, 0, 2) == "//") {
+        $src = $scheme . ":" . $src; 
+    }
+    else if(substr($src, 0, 1) == "/") {
+        $src = $scheme . "://" . $host . $src; 
+    }
+    else if(substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . 
+                substr($src, 1); 
+    }
+    else if(substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src; 
+    }
+    else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+        $src = $scheme . "://" . $host . "/" . $src; 
+    }
+    
+
+    return $src; 
+}
+
+function getDetails($url) {
+    $parser = new DomDocumentParser($url); 
+
+    $titleArray = $parser->getTitleTags(); 
+
+    if(sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
+        return;
+    }
+
+    $title = $titleArray->item(0)->nodeValue; 
+    $title = str_replace("\n", "", $title); 
+
+    if($title == "") {
+        return; 
+    }
+
+    $description = ""; 
+    $keywords = ""; 
+
+    $metaArray = $parser->getMetatags(); 
+
+    foreach($metaArray as $meta) {
+        if($meta->getAttribute("name") == "description") {
+            $description = $meta->getAttribute("content"); 
+        }
+
+        if($meta->getAttribute("name") == "keywords") {
+            $keywords = $meta->getAttribute("content"); 
+        }
+    }
+    $description = str_replace("\n", "", $description); 
+    $keywords = str_replace("\n", "", $keywords); 
+
+
+    echo "URL: $url, Title: $title, Description: $description, Keywords: $keywords<br>";
+}
+
+function followLinks($url) {
+
+    global $alreadyCrawled; 
+    global $crawling;
+
+    $parser = new DomDocumentParser($url); 
+
+    $linkList = $parser->getLinks(); 
+
+    foreach($linkList as $link) {
+        $href = $link->getAttribute("href"); 
+
+        if(strpos($href, "#") !== false) {
+            continue; 
+        }
+        else if(substr($href, 0, 11) == "javascript:") {
+            continue; 
+        }
+
+        $href = createLink($href, $url); 
+
+        if(!in_array($href, $alreadyCrawled)) {
+            $alreadyCrawled[] = $href;
+            $crawling[] = $href;
+
+            getDetails($href); 
+        }
+        else return; 
+
+        // echo $href . "<br>"; 
+    }
+
+    array_shift($crawling);
+
+    foreach($crawling as $site) {
+        followLinks($site);
+
+    }
+
+
+}
+
+$startUrl = "http://bestbuy.com"; 
+// $startUrl = "http://bbc.com"; 
+// $startUrl = "http://msn.com"; 
+// $startUrl = "http://walmart.com"; 
+// $startUrl = "http://newegg.com"; 
+// $startUrl = "http://nintendo.com"; 
+followLinks($startUrl); 
+?>
--- search.php
+++ search.php
@@ -1,3 +1,68 @@
 <?php
-echo "You searched for " . "\"" . $_GET['term'] . "\"";
+
+    if(isset($_GET["term"]) && $_GET["term"] != null) {
+        $term = $_GET["term"];
+    }
+    else {
+        exit("You must enter a search term"); 
+    }
+
+    if(isset($_GET["type"])) {
+        $type = $_GET["type"];
+    }
+    else {
+        $type = "sites"; 
+    }
 ?>
+
+<!DOCTYPE html>
+<head>
+    <title>Welcome to Foogle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/foogle-logo1.png">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer"> 
+                            <input class="searchBox" type="text" name="term">
+                            <button class="searchButton">
+                                <img src="assets/images/icons/search.png">
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+
+            <div class="tabsContainer"> 
+                <ul class="tabsList">
+                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a>
+                    </li>
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Sites</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
